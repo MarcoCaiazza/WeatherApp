ae31fb2194b1424e633f84331beea3d1ed537c04
Marco <Marco@example.com>
2023-03-10 12:36:22 +0100

    creata barra di ricerca html

---
+++ WeatherApp.js
@@ -0,0 +1,7 @@
+// https://api.openweathermap.org/data/2.5/weather?q=Rome&APPID=145746dae4f07b5e4c7d879a1b0431dd
+
+let inputSearch = document.getElementById("search");
+let btnSearch = document.getElementById("bntSearch");
+let weather = document.getElementById("weather");
+
+async function inputValueLinkedUrl() {}
