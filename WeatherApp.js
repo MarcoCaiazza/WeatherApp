3be6ca7f682728c7bd3700de727131d053b77605
Marco <Marco@example.com>
2023-03-11 14:26:07 +0100

    aggiunta ombre testi

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,11 +1,13 @@
-let inputSearch = document.getElementById("search");
-let btnSearch = document.getElementById("btnSearch");
-let loc = document.getElementById("loc");
-let degrees = document.getElementById("degrees");
-let container = document.querySelector(".container");
+const inputSearch = document.getElementById("search");
+const btnSearch = document.getElementById("btnSearch");
+const loc = document.getElementById("loc");
+const degrees = document.getElementById("degrees");
+const container = document.querySelector(".container");
+const iconApp = document.getElementById("iconApp");
+const body = document.querySelector("body");
 
 let url =
-  "https://api.openweathermap.org/data/2.5/weather?APPID=145746dae4f07b5e4c7d879a1b0431dd&q=Rome,It";
+  "https://api.openweathermap.org/data/2.5/weather?APPID=145746dae4f07b5e4c7d879a1b0431dd&q=tokyo";
 
 const createDate = () => {
   let datecurrent = new Date();
@@ -43,23 +45,26 @@ const weatherApp = () => {
     let kelvin = weatherData.main.temp;
     let humidity = weatherData.main.humidity;
     let wind = weatherData.wind.speed;
+    let currentWeather = weatherData.weather[0].main;
+    console.log(currentWeather);
     console.log(wind);
     console.log(humidity);
+    changeIconWeahter(currentWeather);
     kelvinTocelsius(kelvin);
     createElementOfWeather(wind, humidity);
     // kelvinTofahrenheit(kelvin);
   }
   const createElementOfWeather = (wind, humidity) => {
-    let element = document.createElement("element");
+    let element = document.createElement("div");
     element.classList = "allElementWeather";
     container.appendChild(element);
-    let windData = document.createElement("windData");
+    let windData = document.createElement("div");
     windData.classList = "windData";
-    windData.innerHTML = `${wind} km/h`;
+    windData.innerHTML = `Wind Speed : ${wind} km/h`;
     element.appendChild(windData);
-    let humidityData = document.createElement("humidity");
+    let humidityData = document.createElement("div");
     humidityData.classList = "humidityData";
-    humidityData.innerHTML = `Humidity ${humidity} %`;
+    humidityData.innerHTML = `Humidity : ${humidity} %`;
     element.appendChild(humidityData);
   };
 
@@ -73,6 +78,20 @@ const weatherApp = () => {
     fahrenheit = Math.floor(fahrenheit);
     degrees.innerHTML = fahrenheit;
     console.log(fahrenheit);
+  };
+
+  const changeIconWeahter = (currentWeather) => {
+    if (currentWeather === "Clouds") {
+      iconApp.style.backgroundImage = "url('./Images/icons8-clouds-96.png')";
+      body.style.background = "url('./Images/background-cloud-804.jpg')";
+    } else if (currentWeather === "Clear") {
+      iconApp.style.backgroundImage = "url('./Images/icons8-sun-96.png')";
+      // body.style.background = "url('./Images/background-sun-801.jpg')";
+    } else if (currentWeather === "Rain") {
+      iconApp.style.backgroundImage = "url('./Images/icons8-rain-96.png')";
+      body.style.background = "url('./Images/background-rain.jpg')";
+      body.style.color = "white";
+    }
   };
 
   btnSearch.addEventListener("click", (e) => searchLocation(e));
